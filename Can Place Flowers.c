5facd5b599ca965b7f1a8c53ebcc2127cea42981
lovishkathyal <loveshkathyal@gmail.com>
2024-11-20 15:37:31 +0530

    Create Can Place Flowers.c

---
+++ Can Place Flowers.c
@@ -0,0 +1,17 @@
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+    int count = 0;
+
+    for (int i = 0; i < flowerbedSize; i++) {
+        if (flowerbed[i] == 0 &&
+            (i == 0 || flowerbed[i - 1] == 0) &&
+            (i == flowerbedSize - 1 || flowerbed[i + 1] == 0)) {
+            flowerbed[i] = 1;
+            count++;
+        }
+        
+        if (count >= n) {
+            return true;
+        }
+    }
+    return count>= n;
+}
